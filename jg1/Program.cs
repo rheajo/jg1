41c9b94a99e87988113411f346da22a66fd20ef7
rheajo <rheadsouza356@gmail.com>
2021-03-22 14:44:16 +0530

    Add project files.

---
+++ jg1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int c = 101;
+            Console.WriteLine(c);
+        }
+    }
+}
